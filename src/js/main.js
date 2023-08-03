b8b6cd6f8a691178de92f97b1fcaa348e3d57c66
taranvd <apple.vlad.taran@gmail.com>
2023-08-03 16:53:55 +0300

    fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import "./_vendor";
 import vars from "./_vars";
 import "./_functions";
 import "./_components";
-import "../resources/products.json";
+import "../products.json";
 import SimpleBar from "simplebar";
 
 if (document.querySelector(".card")) {
